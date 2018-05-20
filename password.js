540bc3aec0ba76179c23d120a5903b382063e450
HoneyTopaz <39360084+HoneyTopaz@users.noreply.github.com>
2018-05-19 20:36:37 -0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,7 @@
+function checkpass() {
+    var password = document.getElementById('password').value;
+    
+    if (password === 'cookiesforever') {
+        window.location.href = 'code.html';
+    } 
+}
