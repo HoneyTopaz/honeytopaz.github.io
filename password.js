93be5f4d8117f6888791b76e0234d881637640d4
HoneyTopaz <39360084+HoneyTopaz@users.noreply.github.com>
2018-05-28 21:25:45 -0700

    Delete password.js

--- password.js
+++
@@ -1,7 +0,0 @@
-function checkpass() {
-    var password = document.getElementById('password').value;
-    
-    if (password === 'cookiesforever') {
-        window.location.href = 'code.html';
-    } 
-}
